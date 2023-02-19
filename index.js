ae50fd7d6422ed659dccff68b5dae4c09f410dfa
saimun islma <saimunshezan@gmail.com>
2023-02-19 21:05:39 +0600

    second

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { join } from "path"
 import cors from "cors";
 import * as dotenv from "dotenv";
 import dbConnect from "./models/db.js";
@@ -18,7 +17,7 @@ dbConnect();
 //MIDDLEWARE
 app.use(express.json());
 app.use(cors());
-app.use(express.static(join(__dirname,"public/upload")))
+app.use(express.static("public/upload"))
 
 //ROUTER
 app.use("/api/user", userRouter);
